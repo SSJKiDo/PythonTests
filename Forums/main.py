c5a62ca54f03401151b0ee9b3dfdfde1db41085a
KiDo <konvict_massari@yahoo.com>
2018-03-10 18:59:23 +0300

    Started Forum
    Initiated Forum classes.

---
+++ Forums/main.py
@@ -0,0 +1,7 @@
+import models
+
+member1 = models.Member("KiDo", 25)
+member2 = models.Member("Avril", 34)
+post1 = models.Post("Title1", "Content1")
+post2 = models.Post("Title2", "Content2")
+post3 = models.Post("Title3", "Content3")
--- atm solution/atm.py
+++ atm solution/atm.py
@@ -4,6 +4,8 @@ class ATM:
         self.bank_name = bank_name
         self.withdrawals_list = []
         self.withdrawal_time = []
+
+
     def withdraw(self, requested_money, available_currency = [100,50,10,5,1]):
         import datetime
         print "Welcome to " + self.bank_name
@@ -15,6 +17,7 @@ class ATM:
         elif requested_money <= 0:
             print "Wrong amount, try again!"        
         else:
+
             self.withdrawals_list.append(requested_money)
             now = datetime.datetime.now()
             self.withdrawal_time.append(str(now.year)+"-"+str(now.month)+"-"+str(now.day)+" "+str(now.hour)+":"+str(now.minute)+":"+str(now.second))
@@ -28,14 +31,20 @@ class ATM:
             print "Remaining balance = " + str(self.balance)
         print "=" * 30
         return self.balance
+    
     def show_withdrawals(self):
         print "Withdrawals: "
         for withdrawaltime, withdrawal in zip(self.withdrawal_time, self.withdrawals_list):
             print str(withdrawal) + " on " + withdrawaltime
         print "=" * 30
+        
 atm1 = ATM(500, "Smart Bank")
 atm2 = ATM(1000, "Baraka Bank")
 atm3 = ATM(20000, "Tasleef Bank")
 atm1.withdraw(277)
 atm1.withdraw(800)
 atm1.show_withdrawals()
+
+atm2.withdraw(666)
+atm2.withdraw(333)
+atm2.show_withdrawals()
