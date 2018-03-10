0adfd1257437132c8ad1fdf744ee6b8621b4f0a5
KiDo <konvict_massari@yahoo.com>
2018-03-10 16:58:36 +0300

    Enhance Class

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -6,23 +6,23 @@ class ATM:
         print "Welcome to " + self.bank_name
         print "Current balance = " + str(self.balance)
         print "Withdraw = " + str(requested_money)
-        print "=" * 20
+        print "=" * 30
         if requested_money > self.balance:
             print "Can't give you all this money!!!"
         elif requested_money <= 0:
-            print "Wrong amount, try again!"
-        
+            print "Wrong amount, try again!"        
         else:
-            self.balance = self.balance - requested_money
-            avcur = [100,50,10,5,1]
-            for i in avcur:
+            self.balance -= requested_money
+            available_currency = [100,50,10,5,1]
+            for i in available_currency:
                 while requested_money >= i:
                     print "Give " + str(i)
                     requested_money -= i
                 if requested_money == 0:
                     break
             print "Remaining balance = " + str(self.balance)
-        print "=" * 20
+        print "=" * 30
+        return self.balance
 atm1 = ATM(500, "Smart Bank")
 atm2 = ATM(1000, "Baraka Bank")
 atm3 = ATM(20000, "Tasleef Bank")
