bd4ef7b0e3c0114a0ef7af51d299e2c1144439e4
KiDo <konvict_massari@yahoo.com>
2018-03-11 00:42:49 +0300

    Update Main

--- Forums/models.py
+++ Forums/models.py
@@ -1,12 +1,22 @@
-class Member:
+class Member():
+
     def __init__(self, name, age):
+        self.id = 0
         self.name = name
         self.age = age
+        self.posts = []
 
     def __str__(self):
-        return """Name: {}, Age: {}""".format(self.name, self.age)
+        return "Name: {self.name}, Age: {self.age}"
 
-class Post:
-    def __init__(self, title, content):
+
+class Post():
+
+    def __init__(self, title, content, member_id=0):
+        self.id = 0
         self.title = title
         self.content = content
+        self.member_id = member_id
+
+    def __str__(self):
+        return "Title: {self.title}, Content: {self.content}"
